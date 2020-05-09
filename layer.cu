f4ff4627adb09a1404c7e6e2ea6cf3d19a0ee9ae
Hitesh Kumar <rewrihitesh@gmail.com>
2020-05-09 15:51:38 +0530

    Update layer.cu

--- layer.cu
+++ layer.cu
@@ -125,11 +125,11 @@ __global__ void fp_preact_c1(float input[28][28], float preact[6][24][24], float
 	for (int n = N * pos / size; n < N * (pos+1) / size; ++n) {
 		
 		int idx = n;
-		const int i1 = ((idx/1	) % 5);
-		const int i2 = ((idx/5 	) % 5);
-		const int i3 = ((idx/5 	) % 6);
-		const int i4 = ((idx/6 ) % 24);
-		const int i5 = ((idx/24	) % 24);
+		const int i1 = ((idx/=1	) % 5);
+		const int i2 = ((idx/=5	) % 5);
+		const int i3 = ((idx/=5	) % 6);
+		const int i4 = ((idx/=6 ) % 24);
+		const int i5 = ((idx/=24) % 24);
 // 		if(pos==0 || pos==1){
 // 			printf("Thread id:: %d inside for loop :: %d\n", pos,n);
 // 			printf("i1:: %d, i2:: %d, i3:: %d, i4:: %d, i5:: %d \n", i1,i2,i3,i4,i5);
